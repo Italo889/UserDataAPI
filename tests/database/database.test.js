2c4a91c125b7256b61e6c31dae3ed9a925a2f773
stallone-dev <stallone.developer@gmail.com>
2023-12-19 00:33:11 -0300

    test(db): Added basic functional test for Database commands

---
+++ tests/database/database.test.js
@@ -0,0 +1,63 @@
+"use strict";
+
+import { describe, it } from "node:test";
+import { deepStrictEqual, notDeepStrictEqual } from "node:assert";
+import { Database } from "./../../src/database/db.main.js";
+
+describe("Database workflows", () => {
+    let _test_uid = "";
+
+    describe("DB - Create's", () => {
+        let temp_data = {
+            name: "Teste name",
+            email: "test@email.com",
+            age: 123
+        }
+
+        it("Basic creation", async () => {
+            const action = await Database.create(temp_data);
+
+            notDeepStrictEqual(action.uid, undefined);
+            _test_uid = action.uid;
+        });
+
+    });
+
+    describe("DB - consult's", () => {
+        
+        it("Global consult", async () => {
+            const action = await Database.consult();
+
+            deepStrictEqual(Array.isArray(action), true);
+            notDeepStrictEqual(action[0].uid, undefined);
+        });
+
+        it("Unique consult", async () => {
+            const action = await Database.consult(_test_uid);
+
+            deepStrictEqual(action.name, "Teste name");
+            notDeepStrictEqual(action.uid, undefined);
+        });
+    });
+
+    describe("DB - Update's", () => {
+        it("Update data", async () => {
+
+            const new_data = {
+                name: "Teste updated",
+                email: "teste.teste2@email.com",
+                age: 321
+            };
+
+            await Database.update(new_data, _test_uid);
+            deepStrictEqual((await Database.consult(_test_uid)).name, "Teste updated");
+        });
+    });
+
+    describe("DB - Delete's", () => {
+        it("Delete random data", async () => {
+            const action = await Database.delete(_test_uid);
+            deepStrictEqual(action.uid, _test_uid);
+        });
+    });
+})
